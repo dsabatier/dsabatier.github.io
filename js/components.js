a9530346b62cdfabe131e8c1ac7c0eddac9eb6db
Dominic <dsabatier@gmail.com>
2026-03-27 21:15:37 -0600

    cleanup

--- js/components.js
+++ js/components.js
@@ -1,16 +1,19 @@
 class AppHeader extends HTMLElement {
     connectedCallback() {
         const showNav = this.getAttribute('show-nav') !== 'false';
-        
+
         let navHtml = '';
         if (showNav) {
+            const currentPath = window.location.pathname.split('/').pop() || 'index.html';
+
             navHtml = `
             <nav>
                 <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="websites.html">Websites</a></li>
-                    <li><a href="games.html">Games</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="index.html" class="${currentPath === 'index.html' || currentPath === '' ? 'active' : ''}">Home</a></li>
+                    <li><a href="ui-samples.html" class="${currentPath === 'ui-samples.html' ? 'active' : ''}">Portfolio</a></li>
+                    <li><a class="disabled ${currentPath === 'websites.html' ? 'active' : ''}" aria-disabled="true" tabindex="-1">Websites</a></li>
+                    <li><a class="disabled ${currentPath === 'games.html' ? 'active' : ''}" aria-disabled="true" tabindex="-1">Games</a></li>
+                    <li><a href="student-demos.html" class="${currentPath === 'student-demos.html' ? 'active' : ''}">Student Demos</a></li>
                 </ul>
             </nav>
             `;
@@ -39,12 +42,12 @@ class AppFooter extends HTMLElement {
             <a href="#" id="contactLink">Get In Touch?</a>
         </footer>
         `;
-        
+
         const contactLink = this.querySelector('#contactLink');
         const user = 'dsabatier';
         const domain = 'gmail.com';
         const mailto = `mailto:${user}@${domain}`;
-        
+
         if (contactLink) {
             contactLink.href = mailto;
         }
